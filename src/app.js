32af427c711f8fea4b4b6753103af343fdba130c
Amera Abdallah <mera.a.abd@gmail.com>
2018-10-16 00:15:44 +0200

    small change

--- src/app.js
+++ src/app.js
@@ -9,17 +9,9 @@ import getVisibleExpenses from './selectors/visibleExpenses';
 
 import 'normalize.css/normalize.css'; //make style look similar in all browsers
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 500, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-
-
 
 ReactDOM.render(
     <Provider store = {store}>
