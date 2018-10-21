fa758a938dbd7ccb17905bd5a657d05be32bcba9
Amera Abdallah <mera.a.abd@gmail.com>
2018-10-21 22:39:34 +0200

    dummy data file for testing

---
+++ src/tests/fixtures/expenses.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+
+const expenses = [{
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 125,
+    createdAt: 0
+},{
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 109500,
+    createdAt: moment(-90000000000)
+},{
+    id: '3',
+    description: 'Credit Card',
+    note: '',
+    amount: 4500,
+    createdAt: moment(90000000000)
+}];
+
+export default expenses;
