e7e7835c159cb8f8886a2c9b2db59eafa27dabaa
Amera Abdallah <mera.a.abd@gmail.com>
2018-11-05 14:15:23 +0200

    styling expense list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,12 +4,21 @@ import ExpenseListItem from './ExpenseListItem';
 import getVisibleExpenses from '../selectors/visibleExpenses';
 const ExpenseList = (props) => {
     return (
-        <div>
-            <h2>Expense List</h2>
-            {(props.expenses.length > 0) ? props.expenses.map((expense)=>{
-                return <ExpenseListItem key={expense.id} {...expense} />
-            }) : <p>no expenses</p>}
-            
+        <div className="content-container">
+            <div className="list-header">
+                <div className="show-for-mobile">Expenses</div>
+                <div className="show-for-desktop">Expense</div>
+                <div className="show-for-desktop">Amount</div>
+            </div>
+            <div className="list-body">
+                {(props.expenses.length > 0) ? props.expenses.map((expense)=>{
+                    return <ExpenseListItem key={expense.id} {...expense} />
+                }) : (
+                    <div className="list-item list-item--messege">
+                        <span>No Expenses</span>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
